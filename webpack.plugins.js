d7a218d353a8ca4620c28df93fd0100b5dd6a232
Rav Sispal <23012636+rsispal@users.noreply.github.com>
2021-09-22 20:01:06 +0100

    Add polyfills

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,5 +1,7 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = [
+  new NodePolyfillPlugin(),
   new ForkTsCheckerWebpackPlugin()
 ];
